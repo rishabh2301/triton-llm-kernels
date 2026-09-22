646a05dcd94e74fdef019f37282ff0524fbe7386
Rishabh Saraf <30569562+rishabh2301@users.noreply.github.com>
2026-09-22 16:11:32 +0400

    Export kernels package API

---
+++ kernels/__init__.py
@@ -0,0 +1,11 @@
+from .rmsnorm import TritonRMSNorm, rmsnorm, rmsnorm_reference
+from .swiglu import TritonSwiGLUMLP, swiglu, swiglu_reference
+
+__all__ = [
+    "TritonRMSNorm",
+    "rmsnorm",
+    "rmsnorm_reference",
+    "TritonSwiGLUMLP",
+    "swiglu",
+    "swiglu_reference",
+]
